aa1f0e3cf37325a73bc6e08cd7d9ef154a1536bf
Philippe Pepiot <philippe.pepiot@logilab.fr>
2017-09-27 10:47:55 +0200

    WIP enhance summary page
    * Always show the last part of the benchmark name in the left dropdown
    menu (eg. perf -> perf.track_heads become perf -> track_heads)
    * In the summary page, group benchmarks by first component and highlight
    the group name.
    * In summary graphs, show pretty_name or last part of benchmark name

--- asv/www/summarygrid.js
+++ asv/www/summarygrid.js
@@ -21,80 +21,85 @@ $(document).ready(function() {
     function make_summary() {
         var summary_display = $('#summarygrid-display');
         var master_json = $.asv.master_json;
-        var summary_container = $('<div/>');
 
         if (summary_loaded) {
             return;
         }
 
-        $.each(master_json.benchmarks, function(bm_name, bm) {
-            var container = $(
-                '<a class="btn benchmark-container" href="#' + bm_name +
-                '"/>');
-            var plot_div = $(
-                '<div id="summarygrid-' + bm_name + '" class="benchmark-plot"/>');
-            var name = $('<div class="benchmark-text">' + bm_name + '</div>');
-            name.tooltip({
-                title: bm_name,
-                html: true,
-                placement: 'top',
-                container: 'body',
-                animation: false
-            });
+        $.each($.asv.get_benchmark_by_groups(), function(group, benchmarks) {
+            var group_container = $('<div/>');
+            group_container.append($('<h1 style="text-align: center;">' + group + '</h1>'));
+            summary_display.append(group_container);
+            $.each(benchmarks, function(i, bm_name) {
+                var bm = $.asv.master_json.benchmarks[bm_name];
+                var container = $(
+                    '<a class="btn benchmark-container" href="#' + bm_name +
+                    '"/>');
+                var plot_div = $(
+                    '<div id="summarygrid-' + bm_name + '" class="benchmark-plot"/>');
+                var display_name = bm.pretty_name || bm_name.slice(bm_name.indexOf('.') + 1);
+                var name = $('<div class="benchmark-text">' + display_name + '</div>');
+                name.tooltip({
+                    title: bm_name,
+                    html: true,
+                    placement: 'top',
+                    container: 'body',
+                    animation: false
+                });
 
-            plot_div.tooltip({
-                title: bm.code,
-                html: true,
-                placement: 'bottom',
-                container: 'body',
-                animation: false
-            });
+                plot_div.tooltip({
+                    title: bm.code,
+                    html: true,
+                    placement: 'bottom',
+                    container: 'body',
+                    animation: false
+                });
 
-            container.append(name);
-            container.append(plot_div);
-            summary_container.append(container);
+                container.append(name);
+                container.append(plot_div);
+                group_container.append(container);
 
-            callback_in_view(plot_div, function() {
-                $.asv.load_graph_data(
-                    'graphs/summary/' + bm_name + '.json'
-                ).done(function(data) {
-                    var options = {
-                        colors: $.asv.colors,
-                        series: {
-                            lines: {
-                                show: true,
-                                lineWidth: 2
+                callback_in_view(plot_div, function() {
+                    $.asv.load_graph_data(
+                        'graphs/summary/' + bm_name + '.json'
+                    ).done(function(data) {
+                        var options = {
+                            colors: $.asv.colors,
+                            series: {
+                                lines: {
+                                    show: true,
+                                    lineWidth: 2
+                                },
+                                shadowSize: 0
                             },
-                            shadowSize: 0
-                        },
-                        grid: {
-                            borderWidth: 1,
-                            margin: 0,
-                            labelMargin: 0,
-                            axisMargin: 0,
-                            minBorderMargin: 0
-                        },
-                        xaxis: {
-                            ticks: [],
-                        },
-                        yaxis: {
-                            ticks: [],
-                            min: 0
-                        },
-                        legend: {
-                            show: false
-                        }
-                    };
+                            grid: {
+                                borderWidth: 1,
+                                margin: 0,
+                                labelMargin: 0,
+                                axisMargin: 0,
+                                minBorderMargin: 0
+                            },
+                            xaxis: {
+                                ticks: [],
+                            },
+                            yaxis: {
+                                ticks: [],
+                                min: 0
+                            },
+                            legend: {
+                                show: false
+                            }
+                        };
 
-                    var plot = $.plot(
-                        plot_div, [{data: data}], options);
-                }).fail(function() {
-                    // TODO: Handle failure
+                        var plot = $.plot(
+                            plot_div, [{data: data}], options);
+                    }).fail(function() {
+                        // TODO: Handle failure
+                    });
                 });
             });
         });
 
-        summary_display.append(summary_container);
         $(window).scroll();
 
         summary_loaded = true;
